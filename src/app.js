0eed41267d483dd27a49bd778b540002c0456ce6
Matías Rivas <87586371+Dageti@users.noreply.github.com>
2023-10-12 15:31:36 +0000

    corregidos errores, agregadas pintas y funcion para generar aleatoriamente la carta

--- src/app.js
+++ src/app.js
@@ -1,5 +1,38 @@
+import { bottom } from "@popperjs/core";
 import "./style.css";
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  let topcorner = document.querySelector(".top-corner");
+  let bottomcorner = document.querySelector(".bottom-corner");
+  let cardnumber = document.querySelector(".card-number");
+
+  const selectedpinta = RandomPintaGenerator();
+
+  topcorner.classList.add(selectedpinta);
+  bottomcorner.classList.add(selectedpinta);
+  cardnumber.innerHTML = RandomNumberGenerator();
+};
+let RandomNumberGenerator = () => {
+  const possiblenumberpicks = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  const numbertopick = Math.floor(Math.random() * possiblenumberpicks.length);
+  return possiblenumberpicks[numbertopick];
+};
+
+let RandomPintaGenerator = () => {
+  const pinta = ["heart", "diamond", "club", "spade"];
+  const pintatopick = Math.floor(Math.random() * pinta.length);
+  return pinta[pintatopick];
 };
